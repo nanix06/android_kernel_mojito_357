43a63b507b4edcbaea9a5c9705b1b849e08496c8
nanix06 <143353372+nanix06@users.noreply.github.com>
2026-08-15 15:18:48 +0100

    Remove do_execve function from exec.c
    Removed the do_execve function implementation.

--- fs/exec.c
+++ fs/exec.c
@@ -1913,15 +1913,6 @@ int do_execve(struct filename *filename,
 
     return do_execveat_common(AT_FDCWD, filename, argv, envp, 0);
 }
-	
-int do_execve(struct filename *filename,
-	const char __user *const __user *__argv,
-	const char __user *const __user *__envp)
-{
-	struct user_arg_ptr argv = { .ptr.native = __argv };
-	struct user_arg_ptr envp = { .ptr.native = __envp };
-	return do_execveat_common(AT_FDCWD, filename, argv, envp, 0);
-}
 
 int do_execveat(int fd, struct filename *filename,
 		const char __user *const __user *__argv,
