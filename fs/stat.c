19b8e62cd7e9692dff4dfbf7ee691fdd4f8f71c9
nanix06 <143353372+nanix06@users.noreply.github.com>
2026-08-31 18:34:44 +0100

    Add includes for SUSFS support in stat.c

--- fs/stat.c
+++ fs/stat.c
@@ -17,6 +17,10 @@
 #include <linux/syscalls.h>
 #include <linux/pagemap.h>
 #include <linux/compat.h>
+#ifdef CONFIG_KSU_SUSFS
+#include <linux/susfs_def.h>
+#include <linux/version.h>
+#endif
 
 #include <linux/uaccess.h>
 #include <asm/unistd.h>
